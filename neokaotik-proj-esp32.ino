a5c2db4ed2f4bc0715c6e21f5b5e3592499b50c2
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-01 13:54:19 +0100

    feat(NK-121): connect ESP32 to RFID RC522 reader, set up card codes extraction & publish them under 'tower/cardid' topic

--- neokaotik-proj-esp32.ino
+++ neokaotik-proj-esp32.ino
@@ -1,16 +1,25 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+#include <MFRC522DriverPinSimple.h>
 
 // Define the Wi-Fi network credentials
-const char* ssid = "MOVISTAR_D72C"; // Name
-const char* password = "BT5kXJ43awzk2CPwp2vV"; // Password
+const char* ssid = ""; // Name
+const char* password = ""; // Password
 
 // Define the MQTT broker parameters
 const char* mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 
+// Create a partially initialized broker client instance
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// Create elements to work with the MFRC522 RFID reader
+MFRC522DriverPinSimple ss_pin(5);
+MFRC522DriverSPI driver{ss_pin}; // Create SPI driver
+MFRC522 mfrc522{driver}; // Create MFRC522 instance
 
 void setup() {
   // Set software serial baud to 115200
@@ -18,6 +27,7 @@ void setup() {
 
   setUpWiFi();
   setUpBroker();
+  setUpMFRC522();
 }
 
 void setUpWiFi() {
@@ -50,12 +60,19 @@ void processMessages(char* topic, byte* payload, unsigned int length) {
   // if (topicString == "") {}
 }
 
+void setUpMFRC522() {
+  // Init MFRC522 board
+  mfrc522.PCD_Init();
+}
+
 void loop() {
   if (!client.connected()) {
     // Reconnect if the connection to the MQTT broker is lost
     reconnect();
   }
   client.loop(); // Keep the connection alive
+
+  publishRfidCardId();
 }
 
 void reconnect() {
@@ -78,4 +95,29 @@ void reconnect() {
 
   // Subscribe to topics
   // client.subscribe("");
+}
+
+void publishRfidCardId() {
+  // Check if there is a new card &, if so, select it
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    // Save the ID on a String variable & publish it under the topic "tower/cardid"
+
+    String idString = "";
+    
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      if (mfrc522.uid.uidByte[i] < 0x10) {
+        idString += "0";
+      }
+
+      idString += String(mfrc522.uid.uidByte[i], HEX);
+    }
+
+    Serial.print("Card ID: ");
+    Serial.println(idString);
+
+    client.publish("tower/cardid", (char*) idString.c_str());
+
+    // Finish reading the current card
+    mfrc522.PICC_HaltA();
+  }
 }

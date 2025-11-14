711d52a20a3e08108b36d03bef7a16b6233a14f2
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-14 17:27:23 +0100

    refactor: recover code using public broker

--- neokaotik-proj-esp32.ino
+++ neokaotik-proj-esp32.ino
@@ -1,5 +1,4 @@
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <MFRC522v2.h>
 #include <MFRC522DriverSPI.h>
@@ -8,15 +7,15 @@
 #include <ArduinoJson.h>
 
 // Define the Wi-Fi network credentials
-const char* ssid = "IKASLE-LAB"; // Name
-const char* password = "Wb_IKAS_LAB"; // Password
+const char* ssid = ""; // Name
+const char* password = ""; // Password
 
 // Define the MQTT broker parameters
-const char* mqtt_server = "10.50.0.50";
-const int mqtt_port = 8883;
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
 
 // Create a partially initialized broker client instance
-WiFiClientSecure espClient;
+WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Create elements to work with the MFRC522 RFID reader
@@ -44,7 +43,6 @@ void setup() {
   Serial.begin(115200);
 
   setUpWiFi();
-  setUpSSL();
   setUpBroker();
   setUpMFRC522();
   setUpServo();
@@ -65,111 +63,6 @@ void setUpWiFi() {
 
   Serial.println("");
   Serial.println("Connected to the Wi-Fi network");
-}
-
-void setUpSSL() {
-  // Define private key & certificates
-  const char* esp32PrivateKey = \
-    "-----BEGIN PRIVATE KEY-----\n" \
-    "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC3n9nFrK2GVDLL\n" \
-    "bW5F1UiLjffEQw3qTAsIoeUDCSsFyTJIf6UIYZY9dI8/7psPWPHT1uueuSqVCt7c\n" \
-    "lRSO7WIPHbf4ufMoaxmBYuGjKqjHTE0ndoz8/zBYpXRziuH9lUnR7l4j+r3I4w1K\n" \
-    "w9cUtYLCTWFgyGDR5R1qB5mKznKBX9eE8PPa9L7Y/Hvr2nPDtxC6AQo1cBEzLR1d\n" \
-    "LU5jau4fckAxjuE9McwygQMtwvFuLSDAP9k+wcfYb2pht7MxmgX4RkYdZUbhdarl\n" \
-    "Z85hccseY81RXrujx6xKPkFuREm7BRfVSdk/xFTo7pnMN+sy+wcN61mxIz1Tm+1u\n" \
-    "W+bZAnl9AgMBAAECggEACRVlRnwWUXDU2m9dZyL35b9uJE1U4ORGH3UyyQWpFS4w\n" \
-    "yJlX5qQ9NGkRtQ3uqcGypNIa8c0jKCBTigHt9GwVz72enjVErBkwN9BWbSXtF37U\n" \
-    "ykD5FILjN19n9VdHeq3p6vKpwYnj3Y4t2Cb7+tBH2ajOSmVfNMLqw5YAqHcx/uhT\n" \
-    "OwVfXRFCNyliMokpXjXnqxuPpoDAW+Oz1FwS3quN+aRmmse+NGq4KyHOtek6Xbeh\n" \
-    "3cPVcewKDzNn8S+yZ0xdRZjShdUCTinCksCIok9ix0ffYHaFiDFSYwM/t/2UUfpv\n" \
-    "G8Xmza4rwfYoBGYhTGNRebPT7Gq8EucdC+w8ALgiwQKBgQD0zntelCB7Bqq75Pi6\n" \
-    "l9Ql+D4WYNg/6mHvE0UixkWLL/jiJSdutdfmxTQc7FPrbBXbD4Hnfv3nmKX35eM7\n" \
-    "+habP8QqiRJSq3gxU4wRJwQulSohgk3qFvyzkwUIXdDvAK/jkqo01RNMqe3+xaXd\n" \
-    "GmqDgOpFjwiXDLaaST6PqJ4+uQKBgQDABTelMU1LNgDIuCBtkumPPLCSsz0ZaA70\n" \
-    "KqUctb/JzzotUEkMLeQxt6+dyguqVfNMNoeuBIETAPld6AngojYlPF64+yvvYWo6\n" \
-    "hDBvxvtYGdXPKHXkceC4QUfaNcbflF7o6aF96mCxD1CtJLkWRGtpaYaOgen4HKKu\n" \
-    "+g6owxhO5QKBgGo7G8/76WhZiUdF4iVec0ISyiZ92flKZTsQmqSvc0gmBhNkWgsx\n" \
-    "v8G1e5q9duEnIwjPXuis89NHK2Y6jPPe32Z5Mk/e8WOMYvjMr3BWBPuN4R1vZQ4a\n" \
-    "8YefWcS6qUq9epX/c/q/J8PRzaQL8qDSsLvl+kM7AVhCvQ6QkCRY4GqJAoGAOEjP\n" \
-    "OMKsz2Dy9eLSzouDnpLBhx62yB2yVGuznijAIN13RhUEdGd3PgODnJK+vJ0ZBbvu\n" \
-    "JVn4o6NjdN0xGAE0MdHySfKFvFMnmiNpNzpyLi7UdzZ66A1hO2EXOKUOuDZ7Y3Yn\n" \
-    "+gqbcQr2MOpUvmwrt+hQorD97dYm8XcmmN0m4OkCgYBVDWuR2tvp0qcR/R3W/TwO\n" \
-    "kZApa9b41MpDZro1dXecDlEYlJsU9cARCFZ9Kp1JchYWFxHWHDwnNSSCXkrsRO6Y\n" \
-    "h5unz2QaGx+gFWyFqmkZEXqlyZkYCl6iH7VpbsZfQw8mtditCQ7TvoWmIE9oJ74w\n" \
-    "70uE4ZHAOZbpPywInnb16g==\n" \
-    "-----END PRIVATE KEY-----\n"
-  ;
-  const char* esp32Cert = \
-    "-----BEGIN CERTIFICATE-----\n" \
-    "MIIEzjCCAragAwIBAgIUX9V3BHXnAF7wR7yw0QNorrAhCkkwDQYJKoZIhvcNAQEL\n" \
-    "BQAwgYkxCzAJBgNVBAYTAkVVMREwDwYDVQQIDAhHSVBVWktPQTERMA8GA1UEBwwI\n" \
-    "RE9OT1NUSUExDDAKBgNVBAoMA0FFRzEQMA4GA1UECwwHS0FPVElLQTEQMA4GA1UE\n" \
-    "AwwHS0FPVElLQTEiMCAGCSqGSIb3DQEJARYTb3NrYXIuY2Fsdm9AYWVnLmV1czAe\n" \
-    "Fw0yNTExMDcxMDM1NDJaFw0yNjExMDcxMDM1NDJaMGMxCzAJBgNVBAYTAkVTMREw\n" \
-    "DwYDVQQIDAhHaXB1emtvYTERMA8GA1UEBwwIRG9ub3N0aWExDDAKBgNVBAoMA0FF\n" \
-    "RzEQMA4GA1UECwwHS2FvdGlrYTEOMAwGA1UEAwwFRVNQMzIwggEiMA0GCSqGSIb3\n" \
-    "DQEBAQUAA4IBDwAwggEKAoIBAQC3n9nFrK2GVDLLbW5F1UiLjffEQw3qTAsIoeUD\n" \
-    "CSsFyTJIf6UIYZY9dI8/7psPWPHT1uueuSqVCt7clRSO7WIPHbf4ufMoaxmBYuGj\n" \
-    "KqjHTE0ndoz8/zBYpXRziuH9lUnR7l4j+r3I4w1Kw9cUtYLCTWFgyGDR5R1qB5mK\n" \
-    "znKBX9eE8PPa9L7Y/Hvr2nPDtxC6AQo1cBEzLR1dLU5jau4fckAxjuE9McwygQMt\n" \
-    "wvFuLSDAP9k+wcfYb2pht7MxmgX4RkYdZUbhdarlZ85hccseY81RXrujx6xKPkFu\n" \
-    "REm7BRfVSdk/xFTo7pnMN+sy+wcN61mxIz1Tm+1uW+bZAnl9AgMBAAGjUzBRMA8G\n" \
-    "A1UdEQQIMAaHBAoyADIwHQYDVR0OBBYEFDgjcEge2fEkR1qn7IWS7Wqm9JndMB8G\n" \
-    "A1UdIwQYMBaAFOKJUsmG6a1cDtG5IgDP/XkV0ol5MA0GCSqGSIb3DQEBCwUAA4IC\n" \
-    "AQBmJ/1YtZqDsmkrEdL1wGUFGKgRpIUFyBGMQgb0IvXgunFhAZADI1VlmDS4h6hd\n" \
-    "BAqmVmdgPD/LFP7sTucVPAPTD4NU7jRVOv0zCCpxPwm3KX/HQ9BVM4BGvZkt78/p\n" \
-    "ageErW8jffFziAVtkqnDc+VNzlR4VzQHmnN/pdHX/k/LFBzxAhd3VzJD+kjqkmXC\n" \
-    "fCkRmt0z3Mop2NXSJJGGu0jajCNPnRySN0AhUvr5p1aRjc1U9tNvddEo2RG2jkgJ\n" \
-    "6I1visjowrx4sHdQFRxZr/6R9PCXvrLYrFNp0gsR7iiieWolulrm2BO5HoXCQsBy\n" \
-    "fBgGsxLThN8VKqySjA5AUArZAeHP9x/QMrjJEbCKDIXuF7tEC4GS1BTcjcJxYFEz\n" \
-    "h4g4UAjckzZHMBVkRduBfEB28GWRrbbI1hdpvKH0Y6WFA83oT3IlOQvCT94+qZBf\n" \
-    "/ij6hwN/mD1UtJSIicR07UJj6t9qnb1w/ZYfDma7vB4Dr+wOmXwWnFgC/wzXYrqS\n" \
-    "ry7sLiUCn4RMKSV0Qsqsg8eLZnqFcKGUykYpjfni6wKPzmun8i2jRaILN2Sn8GMu\n" \
-    "sOZ3dtttgcIhx7clqWt7LirsoHo/eCL53oyo8k1Ijfy1xsJBahY2CHZhdImKd3Va\n" \
-    "x9d5G8k/v9McSucv+OpRnuG8H5fOEam69Xa799N7IvRhiQ==\n" \
-    "-----END CERTIFICATE-----\n"
-  ;
-  const char* caCert = \
-    "-----BEGIN CERTIFICATE-----\n" \
-    "MIIF9TCCA92gAwIBAgIUTn+K4jmgqp3YxzM+grGiA87ZO1YwDQYJKoZIhvcNAQEL\n" \
-    "BQAwgYkxCzAJBgNVBAYTAkVVMREwDwYDVQQIDAhHSVBVWktPQTERMA8GA1UEBwwI\n" \
-    "RE9OT1NUSUExDDAKBgNVBAoMA0FFRzEQMA4GA1UECwwHS0FPVElLQTEQMA4GA1UE\n" \
-    "AwwHS0FPVElLQTEiMCAGCSqGSIb3DQEJARYTb3NrYXIuY2Fsdm9AYWVnLmV1czAe\n" \
-    "Fw0yNDExMDUxMTA3NDhaFw0zNDExMDMxMTA3NDhaMIGJMQswCQYDVQQGEwJFVTER\n" \
-    "MA8GA1UECAwIR0lQVVpLT0ExETAPBgNVBAcMCERPTk9TVElBMQwwCgYDVQQKDANB\n" \
-    "RUcxEDAOBgNVBAsMB0tBT1RJS0ExEDAOBgNVBAMMB0tBT1RJS0ExIjAgBgkqhkiG\n" \
-    "9w0BCQEWE29za2FyLmNhbHZvQGFlZy5ldXMwggIiMA0GCSqGSIb3DQEBAQUAA4IC\n" \
-    "DwAwggIKAoICAQDRHYLECpA1vHB4oBoqjGn2rJ1vwParc3FIGtBeDDTkKvQWT0rX\n" \
-    "p86a30WGIJW1/AugLqJNyNuU3aiFS/orK1CM2lPe7QP9TwbbW/Jyu5Hnff6f91ZW\n" \
-    "fmzUDsPTI8pCUE0GAz2bsxzA59XGSaMIllbX0cmMUfETU03QdIZOmx7v+fqkX3vY\n" \
-    "sRrDSZ1tuRo+t9MRfidEV17S61/kwDt9WfE56mtREXGr/ogTYnACiA3a5mcsUe84\n" \
-    "tYOKXsMFDV3Xh4iDotPS2pqPHnWhGbculHeFO3NVgsQkt83hHC6df98Tal1OI8cG\n" \
-    "BUTfno6viAa+fSRXVhxRlgVKPcSdqIH9PdU27tEemio7qPrbS3yvInSUt+XtXE0U\n" \
-    "UiPqHQAx8e1bosGFqaOqnGR98YtePo5AwRrl2nTJxbvyhoPg8D0DVAFjzv8UgCHN\n" \
-    "Obo52y1Qb8PqfucFQzltbOVihRmskQbRBg6XORg+hKstDWcZBm6PdgusK2l5lC+H\n" \
-    "gChnlaA22XcSjfqSv7iP5nnU15fXm1L33iZIY9qzKfBg2Zzd8Le4mvn7wGbnKY5/\n" \
-    "AfEyLyxVl/5JHeCnHDGjQayhh6ojZoMR+XgPYmdDqT7OcDAcLemZVPDQvHpeScSq\n" \
-    "ECrPN1J7tbwQsueF9T+DwvbvDPeOcfRAZxgQUico+6q+STWmCQDHaVlAtQIDAQAB\n" \
-    "o1MwUTAdBgNVHQ4EFgQU4olSyYbprVwO0bkiAM/9eRXSiXkwHwYDVR0jBBgwFoAU\n" \
-    "4olSyYbprVwO0bkiAM/9eRXSiXkwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0B\n" \
-    "AQsFAAOCAgEAkQMv07kbzRaG55T6Br0WbtUsDtuDc9m6JWBwaItksPcgDajTgZJc\n" \
-    "71x53Ie94X0JT/wW0MsprD+cUqR6MkHAJpKjSxon7+BxIxF3N/YQbs9dCVDddMqn\n" \
-    "EJ1PhFrmV9Mt/zhhV3p8u8BvuTdgKHtkLhHrwcmSTIl9Ed6YJ+a/dzqkbnTdiEaU\n" \
-    "WZZq7+20mM7xXUxc6uoCqAnyJtDKUV1Gltiff8arEiqJrSa5J6thvD2b3G5ErFID\n" \
-    "lXcj2pEAdH+Ml+Ggp6v0181aeGS/bYQTJXhloYOyudnoSt1X35dvT3HSCsD5T3iT\n" \
-    "cPtu5sX6HJxSrzt5c0Hgm2XHRxGW25XVao1dUn4GqNJ/qMrtqmT9SH4E8Nhse6d4\n" \
-    "H1E362x4RlmsaQbmYNh9toOXqLtBXTjw24qrnprMdSfymiJvJg+u9Itp++m+h87b\n" \
-    "VJPuiFmLeVhfXDHOxl/gvoCgDLfIbwfDQaefEz6WNO898jsciiZn66cCzGu4b+eC\n" \
-    "UGry77DBM08LgoQDBuieDHiMaJ3dgpmvyoWTaVt/oOo2rS1r+o7n9UFW3LgmUiGh\n" \
-    "XKvpsnMBXXMoKEljtahuYWOPqoi+B7wM9YB6wGYnoF8ML3b9XFo9EuJUoRro4JX1\n" \
-    "+F7Gra0qxN7kM9oc6BNlY3GQZwQpG4uRZXwBcp2doptJvSpX/o5F1zY=\n" \
-    "-----END CERTIFICATE-----\n"
-  ;
-
-  // Attach private key & certificates to the client
-  espClient.setPrivateKey(esp32PrivateKey);
-  espClient.setCertificate(esp32Cert);
-  espClient.setCACert(caCert);
 }
 
 void setUpBroker() {
